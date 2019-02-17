98d67d6bfdf1371fa1a75d8574c5682e98eabbd8
r3drighthand <ryanmccool13@gmail.com>
2019-02-17 17:34:15 -0600

    Add code to ReturnRecord class

---
+++ return_record.py
@@ -0,0 +1,7 @@
+class ReturnRecord:
+  def __init__(self, title, author, pages, decimal_number, read_status):
+    self.title = title
+    self.author = author
+    self.pages = pages
+    self.decimal_number = decimal_number
+    self.read_status = read_status
