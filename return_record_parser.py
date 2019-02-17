6643713acb861a1723b3e6f3195db8c7424142e3
r3drighthand <ryanmccool13@gmail.com>
2019-02-17 17:39:49 -0600

    Edit comments

--- return_record_parser.py
+++ return_record_parser.py
@@ -1,8 +1,7 @@
-# Import necessary libraries & files
 import csv
 from record import *
 
-# Store ReturnRecords in list
+# Store Record objects in list
 return_records = []
 
 # Parse target CSV & transform rows into ReturnRecord objects
@@ -10,3 +9,4 @@ with open('return_record.csv') as csv_file:
   csv_reader = csv.reader(csv_file, delimeter=',')
   for row in csv_reader:
     return_records.append(Record(row[0], row[1], row[2], row[3], row[4]))
+
