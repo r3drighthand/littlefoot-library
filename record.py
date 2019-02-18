ba7c5f1ac388b972155502d0cef1e83089032818
r3drighthand <ryanmccool13@gmail.com>
2019-02-18 16:35:24 -0600

    Add pseudo

--- record.py
+++ record.py
@@ -1,34 +1,52 @@
 class Record:
-  def __init__(self, title, author, pages, decimal_number, read_status):
+  def __init__(self, title, author, pages, decimal_category, read_status):
     self.title = title
     self.author = author
     self.pages = pages
-    self.decimal_number = decimal_number
+    self.decimal_category = decimal_category
     self.read_status = read_status
 
-  # Need to refactor long conditional logic
-  def get_dewey_decimal_category(self, decimal_number):
-    if self.decimal_number == 000:
+  def get_dewey_decimal_category(self):
+    # >>> string = str(12345.456)
+    # >>> string
+    # '12345.456'
+    # >>> zero_index = string[0]
+    # >>> zero_index
+    # '1'
+
+    if self.decimal_category == 000:
       return "Computer Science, Informtion, General Works"
-    elif self.decimal_number == 100:
+    elif self.decimal_category == 100:
       return "Philosophy & Psychology"
-    elif self.decimal_number == 200:
+    elif self.decimal_category == 200:
       return "Religion"
-    elif self.decimal_number == 300:
+    elif self.decimal_category == 300:
       return "Social Sciences"
-    elif self.decimal_number == 400:
+    elif self.decimal_category == 400:
       return "Language"
-    elif self.decimal_number == 500:
+    elif self.decimal_category == 500:
       return "Pure Science"
-    elif self.decimal_number == 600:
+    elif self.decimal_category == 600:
       return "Applied Science"
-    elif self.decimal_number == 700:
+    elif self.decimal_category == 700:
       return "Arts & Recreation"
-    elif self.decimal_number == 800:
+    elif self.decimal_category == 800:
       return "Literature"
-    elif self.decimal_number == 900:
+    elif self.decimal_category == 900:
       return "Geography"
     else:
       return "Category Not Found!"
 
-    # print statements are working in REPL
+  # Sanity check:
+  def get_title():
+    print(self.title)
+
+    
+# PSEUDOCODE
+# Grab first integer of DDC & assign variable
+  # Need to convert decimal_category to string?
+  # str.split() delimiter="."
+    # "120.536DUC"
+# Compare variable value with ddc
+  # return correct category
+
