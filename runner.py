989f7b7ebfb94f416d8cba7cb0e248bf431c9ddc
r3drighthand <ryanmccool13@gmail.com>
2019-02-18 20:24:41 -0600

    Add logic to runner

--- runner.py
+++ runner.py
@@ -7,14 +7,32 @@ def count_total_pages_read():
   for record in return_records:
     pages_read = record.get_total_pages_read()
     total_pages += pages_read
-  print(total_pages)
+  print("Total Pages Read: %s" %(total_pages))
 
-# def count_pages_by_category():
-  # loop thru records list
-  # use dictionary to assign category as KEY and pages read for category as VALUE
+def count_pages_by_category():
+  category_dictionary = {}
+  for record in return_records:
+    key = record.get_decimal_category()
+    value = record.get_total_pages_read()
+
+    if key in category_dictionary:
+      category_dictionary[key] += value
+    else:
+      category_dictionary[key] = value
+  
+  # Debugging
+  print(category_dictionary)
 
 
+
+# Formatting --
+# Total Pages Read: number
+# By Category: 
+  # Philosophy: 333
+  # Pure Science: 246
+  # Computer Science: 90
 
 
 
 count_total_pages_read()
+count_pages_by_category()
