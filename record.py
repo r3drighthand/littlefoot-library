3787de8eb972505bde055f22a66376963dcdcd17
r3drighthand <ryanmccool13@gmail.com>
2019-02-24 16:40:09 -0600

    Remove comments & refactor conditional logic in Record

--- record.py
+++ record.py
@@ -8,7 +8,7 @@ class Record:
     self.decimal_category = decimal_category
     self.read = read
 
-  def get_decimal_category(self): # Is this method necessary?
+  def get_decimal_category(self):
     decimal_string = str(self.decimal_category)
     category_indicator = decimal_string[0]
 
@@ -30,10 +30,10 @@ class Record:
       return "Arts & Recreation"
     elif category_indicator == "8":
       return "Literature"
-    else: # Change this to another ELIF for == "9"
+    elif category_indicator == "9":
       return "Geography"
-    # Put an ELSE here in case decimal_string[0] != integer
-    # But why wouldn't it be?
+    else:
+      return "X" # Need error handling
 
   def get_total_pages_read(self):
     read_status = str(self.read)
