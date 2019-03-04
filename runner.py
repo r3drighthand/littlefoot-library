087b312a7dcbcc8ce30736c028811b1d26aaef79
r3drighthand <ryanmccool13@gmail.com>
2019-03-04 17:01:40 -0600

    Refactor runner

--- runner.py
+++ runner.py
@@ -1,59 +1,42 @@
 from return_record_parser import return_records, errors
 
-# Refactor count_pages_by_category and have it return the dictionary object
-# Then write methods that take that dictionary and apply logic to return (or print) the necessary data.
+def run_everything():
+  print_errors()
+  count_total_pages_read()
+  count_pages_by_category()
 
 def get_category_dictionary():
   category_dictionary = {}
-
   for record in return_records:
     key = record.get_decimal_category()
     value = record.get_total_pages_read()
-
     if key in category_dictionary:
       category_dictionary[key] += value 
     else:
       if value > 0:
         category_dictionary[key] = value
-
-  print(category_dictionary)
-
-
-
-########################################################
+  return category_dictionary
 
 def count_total_pages_read(): 
+  dictionary = get_category_dictionary()
   total_pages = 0
-  for record in return_records:
-    pages_read = record.get_total_pages_read()
-    total_pages += pages_read
+  for key in dictionary:
+    total_pages += dictionary[key]
   print "Total Pages Read: %s" %(total_pages)
 
 def count_pages_by_category(): 
-  category_dictionary = {}
-  for record in return_records:
-    key = record.get_decimal_category()
-    value = record.get_total_pages_read()
-
-    if key in category_dictionary:
-      category_dictionary[key] += value 
-    else:
-      if value > 0:
-        category_dictionary[key] = value
-
+  dictionary = get_category_dictionary()
   print "By Category:"
-  print '  ' + '  '.join(['{0}: {1}\n'.format(k, v) for k, v in category_dictionary.iteritems()])
+  print '  ' + '  '.join(['{0}: {1}\n'.format(key, value) for key, value in dictionary.iteritems()])
 
 def print_errors():
   if errors == 1:
-    print "%s error \n" %(errors)
+    print "1 error \n" %(errors)
   elif errors > 1:
     print "%s errors \n" %(errors)
+  else:
+    print "0 errors \n"
 
+####
 
-
-get_category_dictionary()
-
-# print_errors()
-# count_total_pages_read()
-# count_pages_by_category()
+run_everything()
