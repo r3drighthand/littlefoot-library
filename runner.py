b8a24a5109d13a76e75eff689a1fefeee8744fe2
r3drighthand <ryanmccool13@gmail.com>
2019-03-04 16:46:27 -0600

    WIP refactor runner

--- runner.py
+++ runner.py
@@ -1,5 +1,27 @@
 from return_record_parser import return_records, errors
 
+# Refactor count_pages_by_category and have it return the dictionary object
+# Then write methods that take that dictionary and apply logic to return (or print) the necessary data.
+
+def get_category_dictionary():
+  category_dictionary = {}
+
+  for record in return_records:
+    key = record.get_decimal_category()
+    value = record.get_total_pages_read()
+
+    if key in category_dictionary:
+      category_dictionary[key] += value 
+    else:
+      if value > 0:
+        category_dictionary[key] = value
+
+  print(category_dictionary)
+
+
+
+########################################################
+
 def count_total_pages_read(): 
   total_pages = 0
   for record in return_records:
@@ -22,9 +44,6 @@ def count_pages_by_category():
   print "By Category:"
   print '  ' + '  '.join(['{0}: {1}\n'.format(k, v) for k, v in category_dictionary.iteritems()])
 
-  # Refactor count_pages_by_category and have it return the dictionary object
-  # Then write methods that take that dictionary and apply logic to return (or print) the necessary data.
-
 def print_errors():
   if errors == 1:
     print "%s error \n" %(errors)
@@ -33,6 +52,8 @@ def print_errors():
 
 
 
-print_errors()
-count_total_pages_read()
-count_pages_by_category()
+get_category_dictionary()
+
+# print_errors()
+# count_total_pages_read()
+# count_pages_by_category()
